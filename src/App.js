d0509de95f206331dd4d4076c2216de7e6a4e64a
Rehman-942 <rehmanafzal942@gmail.com>
2022-08-03 16:16:50 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./img.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="col-sm-12">
+        <div className="text-center">
+          <div>
+            <img class="col-sm-1" src={logo} alt="Bootstrap Logo" />
+          </div>
+          <div className="sign">
+            <h1>Sign In Page</h1>
+          </div>
+          <div className="para">
+            <p>
+              Build a sign in page to display your command on bootstrap:text
+              with color red pseudo element. Works in latest Chrome, Safari, and
+              Forefox. This is a clickable section that will take us to a new
+              page
+            </p>
+          </div>
+          <div className="inputs">
+            <input id="email" placeholder="Email address"></input>
+            <div>
+              <input id="pswd" placeholder="Password"></input>
+            </div>
+          </div>
+          <div className="remember">
+            <input type="checkbox"></input>
+            <span>Remember me</span>
+          </div>
+          <div className="sign-btn">
+            <button>Sign in</button>
+          </div>
+          <div>&#169 2017-2018</div>
+        </div>
+      </div>
     </div>
   );
 }
